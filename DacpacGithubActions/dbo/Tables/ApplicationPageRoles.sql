c0bcb27ef3998c7fea974b9ccee3e186d7f945eb
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2022-04-18 11:24:52 +0100

    feat: add initial files

---
+++ DacpacGithubActions/dbo/Tables/ApplicationPageRoles.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ApplicationPageRoles] (
+    [Id]       UNIQUEIDENTIFIER NOT NULL,
+    [Name]     NVARCHAR (50)    NOT NULL,
+    [FullName] NVARCHAR (255)   NOT NULL,
+    [List]     BIT              NOT NULL,
+    [Create]   BIT              NOT NULL,
+    [Read]     BIT              NOT NULL,
+    [Edit]     BIT              NOT NULL,
+    [Delete]   BIT              NOT NULL,
+    [RoleName] NVARCHAR (MAX)   DEFAULT (N'') NOT NULL,
+    CONSTRAINT [PK_ApplicationPageRoles] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
